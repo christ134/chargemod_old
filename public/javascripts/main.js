d5f4af427a0f61c9f5cada9353507a25e6d1c355
christ134 <48901442+christ134@users.noreply.github.com>
2019-06-27 04:49:49 +0530

    Add files via upload

---
+++ public/javascripts/main.js
@@ -0,0 +1,23 @@
+function about(){
+    window.location.href='/about';
+}
+
+function Backtohome(){
+    window.location.href='/';
+}
+
+function AlertHello(){
+    alert("Hello! I am an alert box!!");
+}
+
+function hello(){
+    window.location.href='/HelloAlert';
+}
+function button1(){
+    window.location.href='/form';
+}
+function Submit(){
+    const inputdata = document.getElementById('submit').value;
+    alert("you have entered "+ inputdata);
+
+}
